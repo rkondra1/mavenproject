6ab01f87d1a5afc28f5bbc1d54084640554ee9f6
rkondra1 <rajeshcse515@gmail.com>
2018-08-18 09:04:00 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
   * @param someone name of the person
   * @return retuen name of the person
   */
-  public final String greet(final String someone) {
+  public  String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
